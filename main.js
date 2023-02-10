827af3e7c7b8851d11209f56eaab435b9e00f668
Simona Zelenčíková <takendream@gmail.com>
2023-02-10 13:27:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function to_success() {
+    window.location.href = "success.html";
+}
+
+function to_fail1() {
+    window.location.href = "fail1.html";
+}
+
+function to_fail2() {
+    window.location.href = "fail2.html";
+}
